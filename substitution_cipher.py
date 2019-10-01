a99b6ec32ba4152959ad5eae4bb619f1213e3df8
Andy Jornlin <abou123@tuta.io>
2019-10-01 15:32:34 -0700

    added documentation (incl. docstrings), optimized decryption

--- substitution_cipher.py
+++ substitution_cipher.py
@@ -1,36 +1,51 @@
-uppercase_alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+"""this module decrypts a substitution cipher."""
+
+UPPERCASE_ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
                       'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
                       'U', 'V', 'W', 'X', 'Y', 'Z']
 # create a lower-case alphabet from uppercase_alphabet
-lowercase_alphabet = [letter.lower() for letter in uppercase_alphabet]
+LOWERCASE_ALPHABET = [letter.lower() for letter in UPPERCASE_ALPHABET]
 
 
 def part_one(ciphertext, shift_amount, hint=None):
-    # shift_amount can either be the actual shift amount or the first part of
-    # the hint; if it's the first part of the hint (i.e. hint is the other part
-    # of the hint), then calculate the actual shift amount from the difference
-    # between the second and first and second parts of the hint
+    """
+    Decrypts a given ciphertext encrypted with a substitution cipher.
+
+    Takes in the ciphertext and either the actual shift amount or one letter
+    and its shifted counterpart (the shifted character should be farther in the
+    alphabet than the unshifted character).  This function does not support
+    having numbers in the ciphertext.
+    """
     if hint:
         shift_amount = ord(hint) - ord(shift_amount)
     plaintext = ""
 
-    for i in range(len(ciphertext)):
+    # for i in range(len(ciphertext)):
+    #     # if the current character is upper-case
+    #     plaintext += part_one_decode_char(ciphertext[i], shift_amount)
+
+    for i in ciphertext:
         # if the current character is upper-case
-        plaintext += part_one_decode_char(ciphertext[i], shift_amount)
+        plaintext += part_one_decode_char(i, shift_amount)
 
     return plaintext
 
 
 def part_one_decode_char(cipherchar, shift):
+    """
+    Decrypts a single character encrypted with a shift cipher.
+
+    This function takes in the character and the amount the character has been
+    shifted by.
+    """
     # if the letter is upper-case then use uppercase_alphabet for decryption
     if cipherchar.upper() == cipherchar:
-        letter_num = uppercase_alphabet.index(cipherchar)
-        return uppercase_alphabet[(letter_num+shift) % len(uppercase_alphabet)]
+        letter_num = UPPERCASE_ALPHABET.index(cipherchar)
+        return UPPERCASE_ALPHABET[(letter_num+shift) % len(UPPERCASE_ALPHABET)]
 
     # if the letter is lower-case then use lowercase_alphabet for decryption
-    if cipherchar.lower() == cipherchar:
-        letter_num = lowercase_alphabet.index(cipherchar)
-        return lowercase_alphabet[(letter_num+shift) % len(lowercase_alphabet)]
+    letter_num = LOWERCASE_ALPHABET.index(cipherchar)
+    return LOWERCASE_ALPHABET[(letter_num+shift) % len(LOWERCASE_ALPHABET)]
 
 
 print(part_one('BKFTAZDAOWE', 'a', 'o'))
